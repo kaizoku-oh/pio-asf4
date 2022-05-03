759b1766d509c9255ba49314338920fa1161db35
Bayrem GHARSALLAOUI <bayrem.gharsallaoui@sofia-technologies.com>
2022-05-03 19:32:22 +0100

    Add freertos

--- src/main.c
+++ src/main.c
@@ -10,11 +10,15 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#include <FreeRTOS.h>
+#include <task.h>
+
 static void _on_button_pressed(void);
-static void _enable_systick_timer(void);
 static void _on_uart_rx_cplt_cb(const struct usart_async_descriptor *const iODescriptor);
 static void _on_uart_tx_cplt_cb(const struct usart_async_descriptor *const iODescriptor);
 static void _on_uart_error_cb(const struct usart_async_descriptor *const iODescriptor);
+static void _task_1(void *parameters);
+static void _task_2(void *parameters);
 
 #define USART_BUFFER_SIZE 32
 #define BUTTON GPIO(GPIO_PORTA, 15)
@@ -22,18 +26,47 @@ static void _on_uart_error_cb(const struct usart_async_descriptor *const iODescr
 #define VPCOM_UART_TX GPIO(GPIO_PORTA, 22)
 #define VPCOM_UART_RX GPIO(GPIO_PORTA, 23)
 
-static volatile uint32_t ticks = 0;
 static uint8_t usartRxByte = 0;
 static volatile uint8_t usartRxIndex = 0;
 static struct usart_async_descriptor usartDescriptor = {0};
 static uint8_t usartRxBuffer[USART_BUFFER_SIZE] = {0};
 
+void vApplicationStackOverflowHook(TaskHandle_t taskHandle, char *taskName)
+{
+}
+
+void vApplicationMallocFailedHook(void)
+{
+  taskDISABLE_INTERRUPTS();
+  while(1);
+}
+
+void vApplicationIdleHook(void)
+{
+}
+
+static void _task_1(void *parameters)
+{
+  while(1)
+  {
+    io_write(&usartDescriptor.io, (uint8_t *const)"Running task 1\r\n", sizeof("Running task 1\r\n")-1);
+    vTaskDelay(1000);
+  }
+}
+
+static void _task_2(void *parameters)
+{
+  while(1)
+  {
+    io_write(&usartDescriptor.io, (uint8_t *const)"Running task 2\r\n", sizeof("Running task 2\r\n")-1);
+    vTaskDelay(2000);
+  }
+}
+
 int main(void)
 {
   /* Initialize the hardware abstraction layer */
   init_mcu();
-  /* Enable the SysTick timer */
-  _enable_systick_timer();
   /* Configure LED GPIO */
   gpio_set_pin_level(LED_YELLOW, false);
   gpio_set_pin_direction(LED_YELLOW, GPIO_DIRECTION_OUT);
@@ -55,22 +88,16 @@ int main(void)
   usart_async_register_callback(&usartDescriptor, USART_ASYNC_RXC_CB, _on_uart_rx_cplt_cb);
   usart_async_register_callback(&usartDescriptor, USART_ASYNC_ERROR_CB, _on_uart_error_cb);
   usart_async_enable(&usartDescriptor);
+  /* Create FreeRTOS tasks */
+  xTaskCreate(_task_1, "task 1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+  xTaskCreate(_task_2, "task 2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+  /* Start FreeRTOS scheduler */
+  vTaskStartScheduler();
 
   while(1)
   {
-    printf("ticks = %lu\r\n", ticks);
     delay_ms(1000);
   }
-}
-
-static void _enable_systick_timer(void)
-{
-  /* 1. Load the SysTick Counter Value */
-  SysTick->VAL = 0;
-  /* 2. Set the preload register, the value should be your clock frequency in kHz minus 1 */
-  SysTick->LOAD = (CONF_CPU_FREQUENCY / 1000) - 1;
-  /* 3. Enable SysTick exception, SysTick system timer and use processor clock source */
-  SysTick->CTRL = (SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk) | SysTick_CTRL_CLKSOURCE_Msk;
 }
 
 static void _on_button_pressed(void)
@@ -98,26 +125,6 @@ static void _on_uart_error_cb(const struct usart_async_descriptor *const iODescr
 {
 }
 
-void SysTick_Handler(void)
-{
-  ticks++;
-}
-
 void HardFault_Handler(void)
 {
-}
-
-int _write(int fileDesc, char *string, int length)
-{
-  int retVal;
-
-  if((STDOUT_FILENO == fileDesc) || (STDERR_FILENO == fileDesc))
-  {
-    retVal = io_write(&usartDescriptor.io, (uint8_t *)string, length);
-  }
-  else
-  {
-    retVal = -1;
-  }
-  return retVal;
 }
